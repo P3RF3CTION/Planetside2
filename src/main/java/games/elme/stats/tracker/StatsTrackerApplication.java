ef340aa67163d2ffc7b938a36192fe0e1c26c0d9
P3RF3CTION <herzmaximilian@gmail.com>
2019-04-16 22:49:22 +0200

    Initial commit

---
+++ src/main/java/games/elme/stats/tracker/StatsTrackerApplication.java
@@ -0,0 +1,13 @@
+package games.elme.stats.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsTrackerApplication.class, args);
+	}
+
+}
